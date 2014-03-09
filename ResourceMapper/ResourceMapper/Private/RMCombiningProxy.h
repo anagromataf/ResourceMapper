0e75a72d08e6a56c8cbda1e93e95e04c5c71c6ac
Tobias Kräntzer <info@tobias-kraentzer.de>
2014-03-10 00:33:00 +0100

    Add proxy for combining the values of added objects

---
+++ ResourceMapper/ResourceMapper/Private/RMCombiningProxy.h
@@ -0,0 +1,15 @@
+//
+//  RMCombiningProxy.h
+//  ResourceMapper
+//
+//  Created by Tobias Kräntzer on 10.03.14.
+//  Copyright (c) 2014 Tobias Kräntzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMCombiningProxy : NSObject
+
+- (void)addObject:(id)object;
+
+@end
