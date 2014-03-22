893c33bd406f506c8b508e713f4920f80818ce81
Tobias Kräntzer <info@tobias-kraentzer.de>
2014-03-22 17:32:59 +0100

    Add class for the mapping plan

---
+++ ResourceMapper/ResourceMapper/Private/RMMappingStep.h
@@ -0,0 +1,23 @@
+//
+//  RMMappingStep.h
+//  ResourceMapper
+//
+//  Created by Tobias Kräntzer on 22.03.14.
+//  Copyright (c) 2014 Tobias Kräntzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class RMDependency;
+
+@interface RMMappingStep : NSObject
+
++ (NSArray *)mappingStepsWithEntities:(NSSet *)entities
+                           dependency:(RMDependency *)dependency;
+
+#pragma mark Entity & Omitted Relationships
+@property (nonatomic, readonly) NSEntityDescription *entity;
+@property (nonatomic, readonly) NSSet *relationshipsToOmit;
+
+@end
