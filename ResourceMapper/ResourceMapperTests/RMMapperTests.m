68dd1f5a0951cefaa7c5cb5d2ea946065671243a
Tobias Kräntzer <tobias@nxtbgthng.com>
2014-08-15 11:17:59 +0200

    Add properties to the resource mapper, to get some model descriptions regarding primary keys and garbage collection predicates

---
+++ ResourceMapper/ResourceMapperTests/RMMapperTests.m
@@ -0,0 +1,43 @@
+//
+//  RMMapperTests.m
+//  ResourceMapper
+//
+//  Created by Tobias Kräntzer on 15.08.14.
+//  Copyright (c) 2014 Tobias Kräntzer. All rights reserved.
+//
+
+#import "RMManagedObjectModelTestCase.h"
+
+@interface RMMapperTests : RMManagedObjectModelTestCase
+@property (nonatomic, strong) RMMapper *mapper;
+@property (nonatomic, strong) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@end
+
+@implementation RMMapperTests
+
+- (void)setUp
+{
+    [super setUp];
+    self.persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
+    self.mapper = [[RMMapper alloc] initWithPersistentStoreCoordinator:self.persistentStoreCoordinator];
+}
+
+- (void)tearDown
+{
+    self.mapper = nil;
+    self.persistentStoreCoordinator = nil;
+    [super tearDown];
+}
+
+- (void)testMapper
+{
+    [self.mapper.primaryKeyNamesByEntityName enumerateKeysAndObjectsUsingBlock:^(NSString *entityName, NSArray *primaryKeyNames, BOOL *stop) {
+        NSLog(@"%@ -> %@", entityName, [primaryKeyNames componentsJoinedByString:@", "]);
+    }];
+
+    [self.mapper.garbageColelctionPredicatesByEntityName enumerateKeysAndObjectsUsingBlock:^(NSString *entityName, NSPredicate *predicate, BOOL *stop) {
+        NSLog(@"%@ -> %@", entityName, predicate);
+    }];
+}
+
+@end
